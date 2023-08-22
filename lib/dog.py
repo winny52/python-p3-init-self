f1a61e959e24091fe28e942529c44eecff6dddd2
Winny <winny.chelangat2@student.moringaschool.com>
2023-08-22 14:21:26 +0300

    Completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -2,3 +2,6 @@
 
 class Dog:
     pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed =  breed 
--- lib/person.py
+++ lib/person.py
@@ -2,3 +2,6 @@
 
 class Person:
     pass
+    def __init__(self, name):
+        self.name = name
+
